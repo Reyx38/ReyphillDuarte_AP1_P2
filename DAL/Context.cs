260180e7fff79473a2337f65e535a6bc06bab205
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-14 00:10:35 -0400

    clases bases para el parcial

---
+++ DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReyphillDuarte_AP1_P2.Models;
+
+namespace ReyphillDuarte_AP1_P2.DAL
+{
+	public class Context(DbContextOptions<Context> options) : DbContext(options)
+	{
+		public DbSet<Modelo_Examen> Modelo { get; set; }
+		public DbSet<Modelo_Detalles> Detalles { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ Models/Modelo_Examen.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReyphillDuarte_AP1_P2.Models;
+
+public class Modelo_Examen
+{
+	[Key]
+	public int EntidadId { get; set; }
+
+    public string Nombres { get; set; }
+
+    public int Sueldo { get; set; }
+
+	public ICollection<Modelo_Detalles> Detalles { get; set; } = new List<Modelo_Detalles>();
+}
