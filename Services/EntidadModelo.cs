398b6b378647153ff3d78a84dc1d09e938ec2409
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-14 01:57:29 -0400

    plantilla services

---
+++ Services/EntidadModelo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReyphillDuarte_AP1_P2.DAL;
+
+namespace ReyphillDuarte_AP1_P2.Services;
+
+public class ModeloService(IDbContextFactory<Context> DbFactory)
+{
+	public async Task<bool> ExisteModelo() { return  false; }
+	public async Task<bool> Guardar() { return false; }
+	public async Task<bool> Insertar() { return false; }
+	public async Task<bool> Modificar() { return false; }
+	public async Task<bool> Listar() { return false; }
+	public async Task<bool> Buscar() { return false; }
+	public async Task<bool> Eliminar() { return false; }
+
+}
