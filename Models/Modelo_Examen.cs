0dc6f5cb270fbfb03e66fc73f588dbcae7be437c
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-17 20:45:59 -0400

    cambios

--- Models/Modelo_Examen.cs
+++ Models/Modelo_Examen.cs
@@ -7,10 +7,15 @@ public class Modelo_Examen
 	[Key]
 	public int EntidadId { get; set; }
 
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [StringLength(50)]
+    [RegularExpression("^[a-zA-ZÀ-ÿ\\s]+$", ErrorMessage = "Solo se permiten letras.")]
     public string? Nombres { get; set; }
-
-	public DateTime Fecha {  get; set; }
-
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public DateTime Fecha {  get; set; }
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El sueldo por hora debe ser mayor a 0.")]
+    [RegularExpression(@"^\d+$", ErrorMessage = "Solo se permiten números.")]
     public int Sueldo { get; set; }
 
 	public ICollection<Modelo_Detalles> Detalles { get; set; } = new List<Modelo_Detalles>();
