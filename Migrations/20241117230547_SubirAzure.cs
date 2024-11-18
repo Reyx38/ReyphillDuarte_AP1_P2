18e23e1a7b86aefe71a46c7b22ca1ed18e3cf6a0
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-17 20:09:42 -0400

    New migration

---
+++ Migrations/20241117230547_SubirAzure.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReyphillDuarte_AP1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubirAzure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombres",
+                table: "Modelo",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Fecha",
+                table: "Modelo",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fecha",
+                table: "Modelo");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombres",
+                table: "Modelo",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
